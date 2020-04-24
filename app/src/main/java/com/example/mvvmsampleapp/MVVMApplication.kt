4c0d6e119b5c17281fd2a3b313b2a6ababec93d9
MkItSimple <choanania1@gmail.com>
2020-04-24 14:30:31 +0800

    14-DataBindingFragment

--- app/src/main/java/com/example/mvvmsampleapp/MVVMApplication.kt
+++ app/src/main/java/com/example/mvvmsampleapp/MVVMApplication.kt
@@ -6,6 +6,7 @@ import com.example.mvvmsampleapp.data.network.MyApi
 import com.example.mvvmsampleapp.data.network.NetworkConnectionInterceptor
 import com.example.mvvmsampleapp.data.repositories.UserRepository
 import com.example.mvvmsampleapp.ui.auth.AuthViewModelFactory
+import com.example.mvvmsampleapp.ui.home.profile.ProfileViewModelFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
@@ -27,7 +28,7 @@ class MVVMApplication : Application(), KodeinAware {
         bind() from singleton { UserRepository(instance(), instance()) }
         //bind() from singleton { QuotesRepository(instance(), instance(), instance()) }
         bind() from provider { AuthViewModelFactory(instance()) }
-        //bind() from provider { ProfileViewModelFactory(instance()) }
+        bind() from provider { ProfileViewModelFactory(instance()) }
         //bind() from provider{ QuotesViewModelFactory(instance())}
 
 
--- app/src/main/java/com/example/mvvmsampleapp/ui/home/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/home/profile/ProfileViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.mvvmsampleapp.ui.home.profile
 
 import androidx.lifecycle.ViewModel
+import com.example.mvvmsampleapp.data.repositories.UserRepository
 
-class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProfileViewModel(
+    repository: UserRepository
+) : ViewModel() {
+
+    val user = repository.getUser()
+
 }
