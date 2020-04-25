63853827da91b65397f31840059ac4a6a7b885b5
MkItSimple <choanania1@gmail.com>
2020-04-25 10:34:40 +0800

    17-RecyclerViewWithGroupie

--- app/src/main/java/com/example/mvvmsampleapp/ui/home/quotes/QuotesFragment.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/home/quotes/QuotesFragment.kt
@@ -7,9 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmsampleapp.R
+import com.example.mvvmsampleapp.data.db.entities.Quote
 import com.example.mvvmsampleapp.util.Coroutines
+import com.example.mvvmsampleapp.util.hide
+import com.example.mvvmsampleapp.util.show
 import com.example.mvvmsampleapp.util.toast
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.quotes_fragment.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
@@ -33,11 +40,33 @@ class QuotesFragment : Fragment(), KodeinAware {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this, factory).get(QuotesViewModel::class.java)
 
-        Coroutines.main {
-            val quotes = viewModel.quotes.await()
-            quotes.observe(this, Observer {
-                context?.toast(it.size.toString())
-            })
+        bindUI()
+    }
+
+    private fun bindUI() = Coroutines.main {
+        progress_bar.show()
+        viewModel.quotes.await().observe(this, Observer {
+            progress_bar.hide()
+            initRecyclerView(it.toQuoteItem())
+        })
+    }
+
+    private fun initRecyclerView(quoteItem: List<QuoteItem>) {
+
+        val mAdapter = GroupAdapter<ViewHolder>().apply {
+            addAll(quoteItem)
+        }
+
+        recyclerview.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = mAdapter
+        }
+    }
+
+    private fun List<Quote>.toQuoteItem() : List<QuoteItem>{
+        return this.map {
+            QuoteItem(it)
         }
     }
 
